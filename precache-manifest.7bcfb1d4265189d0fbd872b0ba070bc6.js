42f17324eb538dfa27064cf90d8ceacd111a9c22
mjh4ram <munjaehyeon@munjaehyeons-MacBook-Pro.local>
2021-08-26 11:30:16 -0400

    Updates

---
+++ precache-manifest.7bcfb1d4265189d0fbd872b0ba070bc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba275067255ba088eea391a9756e47d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32beb271cd1708f43fe6",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "e3de8203a9559403faa9",
+    "url": "/static/css/main.8ee59f14.chunk.css"
+  },
+  {
+    "revision": "32beb271cd1708f43fe6",
+    "url": "/static/js/2.1cfb8198.chunk.js"
+  },
+  {
+    "revision": "5588a604701b6a13e7f0560374222ee2",
+    "url": "/static/js/2.1cfb8198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3de8203a9559403faa9",
+    "url": "/static/js/main.18c40f55.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/static/media/Inter-Regular.515cae74.ttf"
+  },
+  {
+    "revision": "70d67c3d5126148ee7842b4d82b70301",
+    "url": "/static/media/graph.70d67c3d.png"
+  }
+]);
